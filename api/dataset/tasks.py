b8a3557f3a70d215bd772b09c6e8848247179578
praveencuebok <praveen@cuebook.ai>
2022-01-24 15:00:25 +0530

    small fixes

--- api/dataset/tasks.py
+++ api/dataset/tasks.py
@@ -3,12 +3,13 @@ from celery import shared_task
 from cueSearch.elasticSearch import ESIndexingUtils
 import logging
 
+
 @shared_task
 def indexingJob():
     try:
         logging.info("Indexing starts")
-        ESIndexingUtils.indexGlobalDimensionsDataForSearchSuggestion() # Used for search suggestion
-        ESIndexingUtils.indexAutoGlobalDimensionsDataForSearchSuggestion() #Used for index auto global dimension
+        ESIndexingUtils.indexGlobalDimensionsDataForSearchSuggestion()  # Used for search suggestion
+        ESIndexingUtils.indexNonGlobalDimensionsDataForSearchSuggestion()  # Used for index auto global dimension
         ESIndexingUtils.indexGlobalDimensionName()
         ESIndexingUtils.indexGlobalDimensionsData()
         logging.info("Indexing completed !")
