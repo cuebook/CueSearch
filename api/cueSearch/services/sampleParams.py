e9fa0bbc57a488c5d35a1522e2816447c1d7cd70
iamsuvhro <suvhradip@gmail.com>
2022-02-23 18:29:46 +0530

    Fix the code climate identical blocks issues

--- api/cueSearch/services/sampleParams.py
+++ api/cueSearch/services/sampleParams.py
@@ -1,50 +1,7 @@
 params =  {
-"datasetId": 1,
-"searchResults": [
-    {
-    "value": "BR",
-    "dimension": "DeliveryRegion",
-    "globalDimensionName": "Data8",
-    "user_entity_identifier": "Data8",
-    "id": 8,
-    "dataset": "Test data",
     "datasetId": 1,
-    "type": "GLOBALDIMENSION"
-    },
-    {
-    "value": "GA",
-    "dimension": "DeliveryRegion",
-    "globalDimensionName": "Data8",
-    "user_entity_identifier": "Data8",
-    "id": 8,
-    "dataset": "Test data",
-    "datasetId": 1,
-    "type": "GLOBALDIMENSION"
-    },
-    {
-    "value": "KA",
-    "dimension": "DeliveryRegion",
-    "globalDimensionName": "Data8",
-    "user_entity_identifier": "Data8",
-    "id": 8,
-    "dataset": "Test data",
-    "datasetId": 1,
-    "type": "GLOBALDIMENSION"
-    },
-    {
-    "value": "MH",
-    "dimension": "DeliveryRegion",
-    "globalDimensionName": "Data8",
-    "user_entity_identifier": "Data8",
-    "id": 8,
-    "dataset": "Test data",
-    "datasetId": 1,
-    "type": "GLOBALDIMENSION"
-    }
-],
-"groupedResultsForFilter": [
-    [
-    {
+    "searchResults": [
+        {
         "value": "BR",
         "dimension": "DeliveryRegion",
         "globalDimensionName": "Data8",
@@ -53,8 +10,8 @@ params =  {
         "dataset": "Test data",
         "datasetId": 1,
         "type": "GLOBALDIMENSION"
-    },
-    {
+        },
+        {
         "value": "GA",
         "dimension": "DeliveryRegion",
         "globalDimensionName": "Data8",
@@ -63,8 +20,8 @@ params =  {
         "dataset": "Test data",
         "datasetId": 1,
         "type": "GLOBALDIMENSION"
-    },
-    {
+        },
+        {
         "value": "KA",
         "dimension": "DeliveryRegion",
         "globalDimensionName": "Data8",
@@ -73,8 +30,8 @@ params =  {
         "dataset": "Test data",
         "datasetId": 1,
         "type": "GLOBALDIMENSION"
-    },
-    {
+        },
+        {
         "value": "MH",
         "dimension": "DeliveryRegion",
         "globalDimensionName": "Data8",
@@ -83,29 +40,73 @@ params =  {
         "dataset": "Test data",
         "datasetId": 1,
         "type": "GLOBALDIMENSION"
+        }
+    ],
+    "groupedResultsForFilter": [
+        
+        [
+            {
+                "value": "BR",
+                "dimension": "DeliveryRegion",
+                "globalDimensionName": "Data8",
+                "user_entity_identifier": "Data8",
+                "id": 8,
+                "dataset": "Test data",
+                "datasetId": 1,
+                "type": "GLOBALDIMENSION"
+            },
+            {
+                "value": "GA",
+                "dimension": "DeliveryRegion",
+                "globalDimensionName": "Data8",
+                "user_entity_identifier": "Data8",
+                "id": 8,
+                "dataset": "Test data",
+                "datasetId": 1,
+                "type": "GLOBALDIMENSION"
+            },
+            {
+                "value": "KA",
+                "dimension": "DeliveryRegion",
+                "globalDimensionName": "Data8",
+                "user_entity_identifier": "Data8",
+                "id": 8,
+                "dataset": "Test data",
+                "datasetId": 1,
+                "type": "GLOBALDIMENSION"
+            },
+            {
+                "value": "MH",
+                "dimension": "DeliveryRegion",
+                "globalDimensionName": "Data8",
+                "user_entity_identifier": "Data8",
+                "id": 8,
+                "dataset": "Test data",
+                "datasetId": 1,
+                "type": "GLOBALDIMENSION"
+            }
+            ]
+    ],
+    "filter": "( DeliveryRegion = 'BR' OR DeliveryRegion = 'GA' OR DeliveryRegion = 'KA' OR DeliveryRegion = 'MH' )",
+    "filterDimensions": [
+        "DeliveryRegion"
+    ],
+    "templateSql": "SELECT * FROM ({{ datasetSql|safe }}) WHERE {{filter|safe}} limit 500",
+    "templateTitle": "Dataset = <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{dataset}}</span> where <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{filter}}</span>",
+    "templateText": "This table displays raw data for dataset <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{dataset}}</span> with filter <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{filter}}</span> ",
+    "renderType": "table",
+    "dataset": "Test data",
+    "dimensions": [
+        "DeliveryRegion",
+        "Brand",
+        "WarehouseCode"
+    ],
+    "metrics": [
+        "ReturnEntries",
+        "RefundAmount"
+    ],
+    "timestampColumn": "ReturnDate",
+    "datasetSql": "SELECT DATE_TRUNC('DAY', __time) as ReturnDate,\nDeliveryRegionCode as DeliveryRegion, P_BRANDCODE as Brand, WarehouseCode,\nSUM(\"count\") as ReturnEntries, sum(P_FINALREFUNDAMOUNT) as RefundAmount\nFROM RETURNENTRY\nWHERE __time >= CURRENT_TIMESTAMP - INTERVAL '13' MONTH \nGROUP BY 1, 2, 3, 4\nORDER BY 1",
+    "granularity": "day",
+    "sql": "SELECT * FROM (SELECT DATE_TRUNC('DAY', __time) as ReturnDate,\nDeliveryRegionCode as DeliveryRegion, P_BRANDCODE as Brand, WarehouseCode,\nSUM(\"count\") as ReturnEntries, sum(P_FINALREFUNDAMOUNT) as RefundAmount\nFROM RETURNENTRY\nWHERE __time >= CURRENT_TIMESTAMP - INTERVAL '13' MONTH \nGROUP BY 1, 2, 3, 4\nORDER BY 1) WHERE ( DeliveryRegion = 'BR' OR DeliveryRegion = 'GA' OR DeliveryRegion = 'KA' OR DeliveryRegion = 'MH' ) limit 500"
     }
-    ]
-],
-"filter": "( DeliveryRegion = 'BR' OR DeliveryRegion = 'GA' OR DeliveryRegion = 'KA' OR DeliveryRegion = 'MH' )",
-"filterDimensions": [
-    "DeliveryRegion"
-],
-"templateSql": "SELECT * FROM ({{ datasetSql|safe }}) WHERE {{filter|safe}} limit 500",
-"templateTitle": "Dataset = <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{dataset}}</span> where <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{filter}}</span>",
-"templateText": "This table displays raw data for dataset <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{dataset}}</span> with filter <span style=\"background:#eee; padding: 0 4px; border-radius: 4px;\">{{filter}}</span> ",
-"renderType": "table",
-"dataset": "Test data",
-"dimensions": [
-    "DeliveryRegion",
-    "Brand",
-    "WarehouseCode"
-],
-"metrics": [
-    "ReturnEntries",
-    "RefundAmount"
-],
-"timestampColumn": "ReturnDate",
-"datasetSql": "SELECT DATE_TRUNC('DAY', __time) as ReturnDate,\nDeliveryRegionCode as DeliveryRegion, P_BRANDCODE as Brand, WarehouseCode,\nSUM(\"count\") as ReturnEntries, sum(P_FINALREFUNDAMOUNT) as RefundAmount\nFROM RETURNENTRY\nWHERE __time >= CURRENT_TIMESTAMP - INTERVAL '13' MONTH \nGROUP BY 1, 2, 3, 4\nORDER BY 1",
-"granularity": "day",
-"sql": "SELECT * FROM (SELECT DATE_TRUNC('DAY', __time) as ReturnDate,\nDeliveryRegionCode as DeliveryRegion, P_BRANDCODE as Brand, WarehouseCode,\nSUM(\"count\") as ReturnEntries, sum(P_FINALREFUNDAMOUNT) as RefundAmount\nFROM RETURNENTRY\nWHERE __time >= CURRENT_TIMESTAMP - INTERVAL '13' MONTH \nGROUP BY 1, 2, 3, 4\nORDER BY 1) WHERE ( DeliveryRegion = 'BR' OR DeliveryRegion = 'GA' OR DeliveryRegion = 'KA' OR DeliveryRegion = 'MH' ) limit 500"
-}
